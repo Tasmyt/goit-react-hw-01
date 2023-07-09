14d5f0726a46ef2e065f376ec2f3baaa2b93fe25
Tasmyt <121162710+Tasmyt@users.noreply.github.com>
2023-07-09 08:39:19 +0300

    09 07

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter basename="/goit-react-hw-01">
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
-</BrowserRouter>
+
 );
